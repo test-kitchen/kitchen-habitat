9cb7a299cc15741348de76f625744d4ea4f265eb
Steven Murawski <steven.murawski@gmail.com>
2017-03-02 17:37:03 -0600

    moving to the first release
    Signed-off-by: Steven Murawski <steven.murawski@gmail.com>

--- README.md
+++ README.md
@@ -10,15 +10,45 @@ A Test Kitchen Provisioner for [Habitat](https://habitat.sh)
 You'll need the test-kitchen & kitchen-habitat gems installed in your system, along with kitchen-vagrant or some ther suitable driver for test-kitchen. 
 
 ## Configuration Settings
-* depot_url
+
+### Depot settings
+
+* `depot_url`
   * Target Habitat Depot to use to install packages.
   * Defaults to `nil` (which will use the default depot settings for the `hab` CLI).
-* hab_sup
-  * Package identification for the supervisor to use
+
+### Supervisor Settings
+
+* `hab_sup`
+  * Package identification for the supervisor to use.
   * Defaults to `core/hab-sup`
-* package_name
-  * Package identification for the supervisor to run.
-  * Defaults to the suite name
+* `hab_sup_listen_http`
+  * Port for the supervisor's sidecar to listen on.
+  * Defaults to `nil`
+* `hab_sup_listen_gossip`
+  * Port for the supervisor's gossip communication
+  * Defaults to `nil`
+
+### Package Settings
+
+* `artifact_name`
+  * Artifact package filename to install and run.
+  * Used to upload and test a local artifact.
+  * Example - `core-jq-static-1.5-20170127185151-x86_64-linux.hart`
+  * Defaults to `nil`
+* `package_origin`
+  * Origin for the package to run.
+  * Defaults to `core`, or or, if `artifact_name` is supplied, the `package_origin` will be parsed from the filename of the hart file.
+* `package_name`
+  * Package name for the supervisor to run.
+  * Defaults to the suite name or, if `artifact_name` is supplied, the `package_name` will be parsed from the filename of the hart file.
+* `package_version`
+  * Package version of the package to be run.
+  * Defaults to `nil` or if `artifact_name` is supplied, the `package_version` will be parsed from the filename of the hart file.
+* `package_timestamp`
+  * Package timestamp of the package to be run.
+  * Defaults to `nil` or if `artifact_name` is supplied, the `package_timestamp` will be parsed from the filename of the hart file.
+
 
 ## Example 
 
--- lib/kitchen/provisioner/habitat.rb
+++ lib/kitchen/provisioner/habitat.rb
@@ -24,20 +24,34 @@ module Kitchen
       default_config :hab_sup_listen_gossip, nil
 
       # hab-sup service options
-      #default_config :hab_sup_
+      #default_config :
+      default_config :artifact_name
       default_config :package_origin, "core"
       default_config :package_name do |provisioner|
         provisioner.instance.suite.name
       end
+      default_config :package_version
+      default_config :package_timestamp
 
       default_config :user_toml_path
+
+      def finalize_config!(instance)
+        unless config[:artifact_name].nil?
+          ident = artifact_name_to_package_ident_regex.match(config[:artifact_name])
+          config[:package_origin] = ident["origin"]
+          config[:package_name] = ident["name"]
+          config[:package_version] = ident["version"]
+          config[:package_timestamp] = ident["timestamp"]
+        end
+        super(instance)
+      end
 
       def install_command
         if instance.platform == "windows"
           raise "Need to fill in some implementation here."
         else
           wrap_shell_code <<-BASH
-          #{export_hab_orgin}
+          #{export_hab_origin}
           if command -v hab >/dev/null 2>&1
           then
             echo "Habitat CLI already installed."
@@ -49,7 +63,7 @@ module Kitchen
       end
 
       def init_command
-        wrap_shell_code "id -u hab || sudo useradd hab"
+        wrap_shell_code "id -u hab > /dev/null || sudo useradd hab > /dev/null"
       end
 
       def create_sandbox
@@ -75,30 +89,31 @@ module Kitchen
             echo "Removing dead session."
             sudo screen -wipe > /dev/null
         fi
-        #{export_hab_orgin}
+        #{export_hab_origin}
+        echo "Running #{package_ident}."
         sudo screen -mdS \"#{clean_package_name}\" hab-sup start #{package_ident} #{supervisor_options}
         RUN
 
-        info("Running #{package_ident}.")
         wrap_shell_code run
       end
 
       private
 
       def copy_results_to_sandbox
-        FileUtils.mkdir_p(File.join(sandbox_path, "results/"))
-        FileUtils.cp(
-          File.join(File.dirname(__FILE__), ""),
-          File.join(sandbox_path, ""),
+        FileUtils.mkdir_p(File.join(sandbox_path, "results"))
+        FileUtils.cp_r(
+          File.join(config[:kitchen_root], "/results"),
+          File.join(sandbox_path, "results"),
           preserve: true
         )
       end
 
-      def install_service_package 
-        ""
+      def install_service_package
+        return if config[:artifact_name].nil?
+        "sudo hab pkg install #{File.join(File.join(config[:root_path], "results"), config[:artifact_name])}"
       end
 
-      def export_hab_orgin
+      def export_hab_origin
         return if config[:depot_url].nil?
         "export HAB_ORIGIN=#{config[:depot_url]}"
       end
@@ -111,12 +126,16 @@ module Kitchen
         "sudo hab pkg binlink #{config[:hab_sup]} hab-sup"
       end
 
-      def archive_name_to_package_ident(filename)
-        filename
+      def artifact_name_to_package_ident_regex
+        /(?<origin>\w+)-(?<name>.*)-(?<version>(\d+)?(\.\d+)?(\.\d+)?(\.\d+)?)-(?<timestamp>\d+)-(?<target>.*)\.hart$/
       end
 
       def package_ident
-        @pkg_ident ||= "#{config[:package_origin]}/#{config[:package_name]}"
+        ident = "#{config[:package_origin]}/" \
+                "#{config[:package_name]}/" \
+                "#{config[:package_version]}/" \
+                "#{config[:package_timestamp]}".chomp("/").chomp("/")
+        @pkg_ident ||= ident
       end
 
       def clean_package_name
